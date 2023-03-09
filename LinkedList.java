f9f3cfa25e916ead597f5bdbb044fb69b3b9b6dc
gaurav459433 <gaurav459433@gmail.com>
2023-03-09 19:10:02 +0530

    Welcome To Data Structure Programs

---
+++ LinkedList.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.assignmentsrfp259.day14.datastructureproblems;
+
+public class LinkedList {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Data Structure Programs: ");
+    }
+}
