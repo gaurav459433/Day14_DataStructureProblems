324a321366ad9abecd1e1a832dacdddf2ce296d1
gaurav459433 <gaurav459433@gmail.com>
2023-03-11 18:37:33 +0530

    Added: Code To Create Simple Linked List

--- LinkedList.java
+++ LinkedList.java
@@ -1,7 +1,18 @@
 package com.bridgelabz.assignmentsrfp259.day14.datastructureproblems;
 
 public class LinkedList {
-    public static void main(String[] args) {
-        System.out.println("Welcome To Data Structure Programs: ");
+    public void elements(Object data) {
+        Node newNode = new Node(data);
+        System.out.print(newNode.data + " ");
     }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Linked List Program: ");
+        LinkedList list = new LinkedList();
+        list.elements(56);
+        list.elements(30);
+        list.elements(70);
+    }
+
 }
+
---
+++ Node.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.assignmentsrfp259.day14.datastructureproblems;
+
+public class Node {
+    Object data;
+    Node ref;
+    Node(Object data)
+    {
+        this.data = data;
+    }
+}
