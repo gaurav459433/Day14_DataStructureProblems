5c149a6ed33693983c81c856f855bced08055d38
gaurav459433 <gaurav459433@gmail.com>
2023-03-11 23:23:26 +0530

    Added: Code For Inserting Node Between 2 Nodes

--- LinkedList.java
+++ LinkedList.java
@@ -1,10 +1,13 @@
 package com.bridgelabz.assignmentsrfp259.day14.datastructureproblems;
 
+import java.util.Scanner;
+
 public class LinkedList {
     Node head;
+
     public void addFirst(Object data) {
         Node newNode = new Node(data);
-        newNode.ref = head;
+        newNode.setRef(head);
         head = newNode;
     }
 
@@ -13,43 +16,69 @@ public class LinkedList {
         Node temp = head;
         if (head == null)
             head = newNode;
-        else if (head.ref == null)
-            head.ref = newNode;
+        else if (head.getRef() == null)
+            head.setRef(newNode);
         else {
-            while (temp.ref != null) {
-                temp = temp.ref;
+            while (temp.getRef() != null) {
+                temp = temp.getRef();
             }
-            temp.ref = newNode;
+            temp.setRef(newNode);
         }
+    }
+
+    public void addAtPosition(Object data, int position) {
+        int index = 0;
+        Node newNode = new Node(data);
+        Node left = head;
+        Node right = left.getRef();
+
+        while (index < (position - 1)) {
+            left = left.getRef();
+            right = right.getRef();
+            index++;
+        }
+        newNode.setRef(right);
+        left.setRef(newNode);
     }
 
     public void display() {
         Node temp = head;
         while (temp != null) {
-            if (temp.ref != null)
-                System.out.print(temp.data + " > ");
+            if (temp.getRef() != null)
+                System.out.print(temp.getData() + " > ");
             else
-                System.out.println(temp.data);
-            temp = temp.ref;
+                System.out.println(temp.getData());
+            temp = temp.getRef();
         }
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Linked List Program: ");
         LinkedList list = new LinkedList();
-        System.out.println("Add Nodes at First: ");
-        list.addFirst(70);
-        list.display();
-        list.addFirst(30);
-        list.display();
+//        System.out.println("Add Nodes at First: ");
+//        list.addFirst(70);
+//        list.display();
+//        list.addFirst(30);
+//        list.display();
+//        list.addFirst(56);
+//        list.display();
+//        System.out.println("Add Nodes at Last: ");
+//        list.addLast(56);
+//        list.display();
+//        list.addLast(30);
+//        list.display();
+//        list.addLast(70);
+//        list.display();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Add Node Between 2 Nodes: ");
         list.addFirst(56);
         list.display();
-        System.out.println("Add Nodes at Last: ");
-        list.addLast(56);
-        list.display();
-        list.addLast(30);
-        list.display();
         list.addLast(70);
+        list.display();
+        System.out.println("Enter 1 To Add Node 30 Between 56 & 70 : ");
+        int position = sc.nextInt();
+        list.addAtPosition(30, position);
         list.display();
     }
 }
--- Node.java
+++ Node.java
@@ -1,10 +1,26 @@
 package com.bridgelabz.assignmentsrfp259.day14.datastructureproblems;
 
 public class Node {
-    Object data;
-    Node ref;
+    private Object data;
+    private Node ref;
     Node(Object data)
     {
         this.data = data;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public Node getRef() {
+        return ref;
+    }
+
+    public void setRef(Node ref) {
+        this.ref = ref;
     }
 }
